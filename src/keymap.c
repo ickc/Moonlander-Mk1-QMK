c86b3cf029ed4e406d8e1f0b353b6e8bf2fc58ef
Kolen Cheung <christian.kolen@gmail.com>
2025-11-20 20:03:02 +0000

    enable chordal hold; exclude thumb keys from Chordal Hold (patched)

--- src/keymap.c
+++ src/keymap.c
@@ -64,8 +64,8 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
   'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
   'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
-  'L', 'L', 'L', 'L', 'L', '*', '*', 'R', 'R', 'R', 'R', 'R',
-                 '*', '*', '*', '*', '*', '*'
+  'L', 'L', 'L', 'L', 'L', 'R', 'L', 'R', 'R', 'R', 'R', 'R',
+                 'R', 'R', 'R', 'L', 'L', 'L'
 );
 
 
