6a8cb1cb067af2b1178d38c07d0a8248b1be8a01
Kolen Cheung <christian.kolen@gmail.com>
2025-11-20 19:30:44 +0000

    enable chordal hold; exclude thumb keys from Chordal Hold

--- src/config.h
+++ src/config.h
@@ -1,3 +1,4 @@
+#define CHORDAL_HOLD
 #define PERMISSIVE_HOLD
 
 #undef RGB_MATRIX_TIMEOUT
@@ -5,7 +6,7 @@
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define HOLD_ON_OTHER_KEY_PRESS
-#define SERIAL_NUMBER "QwA3z/v65q0b"
+#define SERIAL_NUMBER "QwA3z/nlPBQ0"
 #define LAYER_STATE_8BIT
 #define HSS(report) host_system_send(record->event.pressed ? report : 0); return false
 #define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
